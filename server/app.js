5831cb5803440c3963ca556dcb320fb70d79a665
Silvano Filho <silvarjf@gmail.com>
2019-03-01 12:28:03 -0300

    Adicionando request ID

--- server/app.js
+++ server/app.js
@@ -3,29 +3,54 @@
  */
 
 var express = require('express');
+var addRequestId = require('express-request-id')();
+var fs = require('fs');
 var cmd = require("child_process").execSync;
 var app = express();
-var config = require('config')
-var wgrib2 = config.get("wgrib2")
+var config = require('config');
+var wgrib2 = config.get("wgrib2");
+var userRequestsFolder = config.get('userRequestsFolder');
+
+app.use(addRequestId);
+
+if (fs.existsSync(userRequestsFolder)) {
+	cmd("rm -rf " + userRequestsFolder)
+}
+
+fs.mkdirSync(userRequestsFolder);
+
+
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 
-	console.log(JSON.parse(req.query))
+	console.log(req.query.lat)
 
+	// TODO: Consistência da chamada
 	if (req.query.lat && req.query.lon && req.query.day) {
 		console.log("nice")
-		var lat = req.query.lat.min
-		var lon = req.query.lon.min
-		var day = req.query.day.min
 
-		var command = ' ../data/years/2019.grib2 -match "d=' + day + '" -undefine out-box ' + lon + ':' + lon + ' ' + lat + ':' + lat + '-csv teste.csv'
+
+		// TODO: Verificar se são limites ou datas certas
+		var lat = req.query.lat
+		var lon = req.query.lon
+		var day = req.query.day
+
+		// TODO: Converter a data pra uma lógica de abrir vários arquivos anuais
+		/* TODO: Pra cada arquivo anual, colocar as expressões regulares (por mês se forem meses inteiros, por dias se não forem)
+		 */
+		// TODO: As coordenadas não são exatas. São -0.5*step:+0.5*step
+
+		var command = wgrib2 + ' ../data/years/2019.grib2 -match "d=' + day + '" -undefine out-box ' + lon + ':' + lon + ' ' + lat + ':' + lat + ' -csv ' + userRequestsFolder + req.id + '.csv'
 		console.log(command)
 
 		cmd(command)
+
+		// TODO: Juntar todos os arquivos e mandar pro usuário
+
 	}
 
-	console.log(req.query)
+	console.log(req.id)
 });
 
 app.listen(3000, function () {
